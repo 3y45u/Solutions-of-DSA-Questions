1c6304dd7f25565e689e6aa33bac977a0ff575c7
Gagan Raturi <62871267+3y45u@users.noreply.github.com>
2021-05-28 02:46:49 +0530

    Update Solution.java

--- dsa study/LinkedList/Palindrome list/Solution.java
+++ dsa study/LinkedList/Palindrome list/Solution.java
@@ -1,3 +1,7 @@
+// This code is the basic idea of checking if a linked list is palindrome or not
+// The linked list is divided into to parts and the next half of list is reversed 
+// if both the parts of list are equal it is a palindrome
+// this is the one of the short method 
 class Solution {
     public boolean isPalindrome(ListNode head) {
         ListNode slow = head;
